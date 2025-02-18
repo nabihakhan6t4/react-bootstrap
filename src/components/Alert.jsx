17c679f2db316ffdd347a315f6d59db5dc8edb1f
nabihakhan6t4 <khannabiha923@gmail.com>
2025-02-18 17:44:28 +0300

    alert smoothly pened

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,15 +1,27 @@
+import { motion, AnimatePresence } from "framer-motion";
 import PropTypes from "prop-types";
+import "../components/Alert.css"; // Import CSS
 
 const Alert = ({ alert }) => {
   return (
-    alert && (
-      <div className={`alert alert-${alert.type} alert-dismissible fade show`} role="alert">
-        <strong>{alert.type.toUpperCase()}:</strong> {alert.msg}
-      </div>
-    )
+    <div className="alert-container">
+      <AnimatePresence>
+        {alert && (
+          <motion.div
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -20 }}
+            transition={{ duration: 0.5 }}
+            className={`alert alert-${alert.type} alert-dismissible fade show`}
+            role="alert"
+          >
+            <strong>{alert.type.toUpperCase()}:</strong> {alert.msg}
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </div>
   );
 };
-
 
 Alert.propTypes = {
   alert: PropTypes.shape({
